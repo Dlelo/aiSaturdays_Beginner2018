77835f8d4ba69c758f89841373074621c9bb6fa3
dlelo <akinyidonattah@gmail.com>
2018-07-30 17:04:07 +0300

    ai Surtudays_beginner2018 challege 1 and challenge 2

--- task1.py
+++ task1.py
@@ -1,9 +1,15 @@
 import random 
 
 ourList = list()
+belowFive = list()
 count = 0 
 while (count < 11):
     ourList.append(random.randint(1,10))
     count += 1
-    
-ourList
+  
+print "Random numbers between 1 and 10" , ourList
+for num in ourList:
+	if num<5:
+		belowFive.append(num)
+print "Random numbers below 5. Answer: "
+print belowFive
--- task2.py
+++ task2.py
@@ -3,4 +3,19 @@ import random
 words = [word.rstrip('\n') for word in open('words.txt')]
 randomPhrase = " ".join([words[random.randrange(0, len(words))] for i in range(4)])
 
-randomPhrase
+print "The 4 words: " 
+print randomPhrase
+splitingPhrase=randomPhrase.split(" ")
+
+reversingPhrase=splitingPhrase[-1::-1]
+joinReversePhrase = ' '.join(reversingPhrase)
+#print joinReversePhrase
+#reverse order of the words
+
+
+reverseWords = joinReversePhrase.split(" ")
+thereversedWords = [words[::-1] for words in reverseWords]
+reversePhrase = " ". join(thereversedWords)
+
+print "Reverse the order of the 4 words and the letters in each word. Answer: "
+print reversePhrase
